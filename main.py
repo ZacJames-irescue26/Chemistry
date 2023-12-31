d902db481a9abcbf38982ea154d8a867b2c18d94
Zac james <zac.james016@gmail.com>
2023-12-31 20:00:03 +0000

    started Textures

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ctypes import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 import imgui
 from imgui.integrations.glfw import GlfwRenderer
+from PIL import Image
 
 class App:
     def __init__(self):
@@ -28,7 +29,9 @@ class App:
         glClearColor(0.1, 0.2, 0.2, 1)
         self.shader = self.CreateShader("Shaders/vertex.txt", "Shaders/fragment.txt")
         glUseProgram(self.shader)
+        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
         self.triangle = Triangle()
+        self.texture = Material("Textures/container2.png")
         imgui.create_context()
         self.impl = GlfwRenderer(self.window)
         self.show_custom_window = True
@@ -44,6 +47,7 @@ class App:
             self.impl.process_inputs()
             glClear(GL_COLOR_BUFFER_BIT)
             glUseProgram(self.shader)
+            self.texture.use()
             glBindVertexArray(self.triangle.vao)
             glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex_count)
             imgui.new_frame()
@@ -85,6 +89,7 @@ class App:
 
     def quit(self):
         self.triangle.destroy
+        self.texture.destroy()
         glDeleteProgram(self.shader)
         glfw.terminate()
 
@@ -118,10 +123,11 @@ class App:
 
 class Triangle:
     def __init__(self):
+        # x,y,z,r,g,b,s,t
         self.vertices = (
-            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-            0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-            0.0, 0.5, 0.0, 0.0, 0.0, 1.0
+            -0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0,
+            0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0,
+            0.0, 0.5, 0.0, 0.0, 0.0, 1.0, 0.5, 0.0
         )
         self.vertices = np.array(self.vertices, dtype=np.float32)
 
@@ -133,15 +139,46 @@ class Triangle:
         glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
         glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(0))
         glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
-    
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
+        glEnableVertexAttribArray(2)
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(24))
+
     def destroy(self):
         glDeleteVertexArrays(1, (self.vao,))
         glDeleteBuffers(1, (self.vbo,))
 
+class Material:
+    def __init__(self, filenames):
 
+        texture_size = 500
+        texture_count = len(filenames)
+        width = 5 * texture_size
+        height = texture_count * texture_size
+        self.textureData = Image.new(mode = "RGBA", size = (width, height))
+        for i in range(texture_count):
+            with Image.open(f"{filenames}", mode = "r") as img:
+                img = img.convert("RGBA")
+                self.textureData.paste(img, (0, (texture_count - i - 1) * texture_size))
+            ## add more images here
+        img_data = bytes(self.textureData.tobytes())
+        self.texture = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        glGenerateMipmap(GL_TEXTURE_2D)
+    
+    def use(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+    def destroy(self):
+        glDeleteTextures(1, (self.texture,))
+        
 def main():
     app = App()
 
